8dd731f55f1441d9bc6ffafa5ad17742e8165e01
Maria Szczerba <mariaszczerba00@gmail.com>
2023-11-10 21:33:51 +0100

    Task 4, pulsar queue working

--- mock_data_hw/consumer.py
+++ mock_data_hw/consumer.py
@@ -1,3 +1,7 @@
+"""
+In order to run source queue task we need to open two WSL terminals.
+Then we in one we run firstly consumer.py file and in the other one we run consumer.py file.
+"""
 from pulsar import Client, ConsumerType
 
 SERVICE_URL = "pulsar://localhost:6650"
